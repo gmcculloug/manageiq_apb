1beef69cba5b00670341b2fb99bce7b7c147ce1a
Greg McCullough <gmccullo@redhat.com>
2018-05-07 13:22:34 -0700

    Add APB push to setup script Remove/Uninstall ansible-service-broker on start Make setup script more generic Change approver URLs and names

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # manageiq_apb
 
+Setup:
+`source ./minishift_config/setup.sh`
+
 Example:
 
  `ruby miq_apb_tool/service_template_to_apb.rb -s https://10.8.198.132 -t LAMP-Stack -n`
--- minishift_config/setup.sh
+++ minishift_config/setup.sh
@@ -1,4 +1,10 @@
+INITIAL_DIR=$PWD
+MINISHIFT_CONFIG_DIR=$(dirname $0)
+
+minishift addons remove ansible-service-broker
+minishift addons uninstall ansible-service-broker
 minishift delete
+
 minishift config set memory 4096
 minishift config set cpus 4
 minishift config set disk-size 40g
@@ -9,25 +15,34 @@ minishift config set iso-url centos
 minishift start --vm-driver=virtualbox --extra-clusterup-flags "--service-catalog"
 # minishift start --extra-clusterup-flags "--service-catalog"
 
-eval $(minishift docker-env)
-eval $(minishift oc-env)
-docker login 172.30.1.1:5000 -u unused -p $(oc whoami -t)
-
-minishift addons uninstall ansible-service-broker
-minishift addons install ~/work/Summit/minishift-addons/add-ons/ansible-service-broker/
-sleep 10
-minishift addons apply ansible-service-broker
-
-oc create -f "$(dirname "$0")/projects"
-
 oc login -u system:admin
 oc adm policy add-cluster-role-to-user cluster-admin developer
 oc login -u developer -p smartvm
+
+eval $(minishift docker-env)
+printenv DOCKER_HOST
+eval $(minishift oc-env)
+docker login 172.30.1.1:5000 -u unused -p $(oc whoami -t)
+
+minishift addons install "$MINISHIFT_CONFIG_DIR/../../minishift-addons/add-ons/ansible-service-broker/"
+sleep 10
+minishift addons apply ansible-service-broker
+
+cd "$MINISHIFT_CONFIG_DIR/../lamp-stack-apb"
+apb push
+
+cd "$MINISHIFT_CONFIG_DIR/../hadoop-cluster-apb"
+apb push
+
+cd "$MINISHIFT_CONFIG_DIR/../centos7-satellite-apb hadoop-cluster-apb"
+apb push
+
+oc create -f "$(dirname "$0")/projects"
 
 # oc label user/developer permissions=Requires-Approval
 # oc label namespace myproject permissions=Requires-Approval
 # oc describe namespace myproject
 # oc edit oauthclient openshift-web-console
-# oc login -u developer -p dev
 
 minishift console
+cd $INITIAL_DIR
