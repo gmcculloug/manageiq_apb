a7629a6ad270f6021c1a5c41c8f656ed8d3f1761
Greg McCullough <gmccullo@redhat.com>
2018-05-07 22:10:07 -0700

    Clone minishift-addons repo and switch to add-dashboard-url branch. Require key press before installing APBs

--- minishift_config/setup.sh
+++ minishift_config/setup.sh
@@ -24,9 +24,23 @@ printenv DOCKER_HOST
 eval $(minishift oc-env)
 docker login 172.30.1.1:5000 -u unused -p $(oc whoami -t)
 
-minishift addons install "$MINISHIFT_CONFIG_DIR/../../minishift-addons/add-ons/ansible-service-broker/"
+oc create -f "$(dirname "$0")/projects"
+minishift console
+printenv DOCKER_HOST
+
+cd $MINISHIFT_CONFIG_DIR
+git clone https://github.com/eriknelson/minishift-addons
+cd minishift-addons
+git checkout add-dashboard-url
+cd ../..
+
+minishift addons install "$MINISHIFT_CONFIG_DIR/minishift-addons/add-ons/ansible-service-broker/"
 sleep 10
 minishift addons apply ansible-service-broker
+# minishift addons enable ansible-service-broker
+
+echo "Wait for Ansible Service Broker to start then press the Enter Key to continue installing APBs"
+read
 
 cd "$MINISHIFT_CONFIG_DIR/../lamp-stack-apb"
 apb push
@@ -37,12 +51,10 @@ apb push
 cd "$MINISHIFT_CONFIG_DIR/../centos7-satellite-apb hadoop-cluster-apb"
 apb push
 
-oc create -f "$(dirname "$0")/projects"
-
 # oc label user/developer permissions=Requires-Approval
 # oc label namespace myproject permissions=Requires-Approval
 # oc describe namespace myproject
 # oc edit oauthclient openshift-web-console
 
-minishift console
+printenv DOCKER_HOST
 cd $INITIAL_DIR
